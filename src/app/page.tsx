6b4a269e45ace78909c388a26e1493aa2816c13e
Manish Ghimire <mannish079@gmail.com>
2025-04-04 16:18:36 +0545

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
-import Hero from "./landing/heroSection";
-import FeaturesSection from "./landing/features";
-import Testimonials from "./landing/testimonials";
-import AboutUs from "./landing/about";
-import CTA from "./landing/CTA";
+// import Hero from "./landing/heroSection";
+// import FeaturesSection from "./landing/features";
+// import Testimonials from "./landing/testimonials";
+// import AboutUs from "./landing/about";
+// import CTA from "./landing/CTA";
+import ComingSoon from "./comingSoon"
 export default function Home() {
   return (
     <>
-        <Hero />
+        {/* <Hero />
         <AboutUs />
         <FeaturesSection />
         <Testimonials />
-        <CTA />
+        <CTA /> */}
+      <ComingSoon />
     </>
   );
 }
