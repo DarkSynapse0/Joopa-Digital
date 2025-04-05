c3674317c795a653ba5a9d41e56af81b94ed18a8
Manish Ghimire <mannish079@gmail.com>
2025-04-05 19:39:16 +0545

    removed header and footer

--- src/app/coming/ComingSoon.tsx
+++ src/app/coming/ComingSoon.tsx
@@ -3,33 +3,6 @@
 import { useState, useEffect } from "react";
 
 const ComingSoon = () => {
-  const calculateTimeLeft = () => {
-    const launchDate = new Date("2025-06-01T00:00:00").getTime(); // Adjust date
-    const now = new Date().getTime();
-    const timeDifference = launchDate - now;
-
-    let timeLeft = {};
-    if (timeDifference > 0) {
-      timeLeft = {
-        days: Math.floor(timeDifference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((timeDifference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((timeDifference / (1000 * 60)) % 60),
-        seconds: Math.floor((timeDifference / 1000) % 60),
-      };
-    }
-
-    return timeLeft;
-  };
-
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
-    return () => clearInterval(timer);
-  }, []);
-
   return (
     <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
       <h1 className="text-5xl text-white font-bold mb-8 animate-pulse">
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,9 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header>{<Navbar />}</header>
+        {/* <header>{<Navbar />}</header> */}
         {children}
-        <footer>{<Footer />}</footer>
+        {/* <footer>{<Footer />}</footer> */}
       </body>
     </html>
   );
