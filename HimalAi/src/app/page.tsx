cff3fea21213401427f06f7924c3fadf9fed5a4b
Manish Ghimire <mannish079@gmail.com>
2025-04-15 07:04:55 +0545

    removed himal ai

--- HimalAi/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import AdminPanel from "./adminpanel/page";
-
-export default function Home() {
-  return (
-    <>
-      <AdminPanel />
-    </>
-  );
-}
--- HimalAi/src/config/api.tsx
+++
@@ -1,2 +0,0 @@
-const API_BASE_URL = "https://1xkj47zz-3000.inc1.devtunnels.ms";
-export default API_BASE_URL;
