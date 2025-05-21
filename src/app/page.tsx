8067417cf1fccb09bb3543953617703108afb337
Manish Ghimire <mannish079@gmail.com>
2025-05-21 07:50:06 +0545

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-// import Hero from "./landing/heroSection";
-// import FeaturesSection from "./landing/features";
-// import Testimonials from "./landing/testimonials";
-// import AboutUs from "./landing/about";
-// import CTA from "./landing/CTA";
-import ComingSoon from "./coming/ComingSoon";
+import Hero from "./landing/heroSection";
+import FeaturesSection from "./landing/features";
+import Testimonials from "./landing/testimonials";
+import AboutUs from "./landing/about";
+import CTA from "./landing/CTA";
+// import ComingSoon from "./coming/ComingSoon";
 
 export default function Home() {
   return (
